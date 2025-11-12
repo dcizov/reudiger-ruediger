d1c0bf296c66cf65dede9cbc4a07aa915c5569d9
dcizov <dimitricizov30@gmail.com>
2025-11-13 00:37:06 +0100

    chore(config): update vscode, eslint, and typescript configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,37 @@
-import drizzle from "eslint-plugin-drizzle";
-import { defineConfig } from "eslint/config";
-import tseslint from "typescript-eslint";
+import drizzle from 'eslint-plugin-drizzle';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 
 export default defineConfig(
   {
-    ignores: ["node_modules/**", "dist/**"],
+    ignores: [
+      'node_modules/**',
+      'dist/**',
+      '*.config.mjs',
+      '*.config.js',
+      'commitlint.config.mjs',
+    ],
   },
   ...tseslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
-    files: ["src/**/*.ts", "src/**/*.tsx"],
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
     plugins: {
       drizzle,
     },
     rules: {
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_' },
       ],
-      "drizzle/enforce-delete-with-where": [
-        "error",
-        { drizzleObjectName: ["db", "ctx.db"] },
+      'drizzle/enforce-delete-with-where': [
+        'error',
+        { drizzleObjectName: ['db', 'ctx.db'] },
       ],
-      "drizzle/enforce-update-with-where": [
-        "error",
-        { drizzleObjectName: ["db", "ctx.db"] },
+      'drizzle/enforce-update-with-where': [
+        'error',
+        { drizzleObjectName: ['db', 'ctx.db'] },
       ],
     },
   },
@@ -38,5 +44,5 @@ export default defineConfig(
         projectService: true,
       },
     },
-  }
+  },
 );
